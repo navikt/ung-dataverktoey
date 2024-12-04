d4dd8ee2762d0b6b9bc0aba97ab4340a6acd7f66
hellund <eirik.duesund.helland@nav.no>
2024-12-04 12:10:29 +0100

    Legg til plotteverktøy

---
+++ src/ung_plotteverktoey/data.py
@@ -0,0 +1,354 @@
+import pandas as pd
+import numpy as np
+from pypalettes import load_cmap
+from typing import List, Dict
+import pickle
+import random
+
+
+class HighChartData:
+    def __init__(self, 
+                 filnavn: str, 
+                 kolonner: List[str], 
+                 svar_alternativer: List[str],
+                 ):
+        self.filnavn = filnavn
+        self.kolonner = kolonner
+        self.svar_alternativer = svar_alternativer
+        self.dataserier = self.lag_dataserier()
+
+    def normaliser_kolonne(self, kolonne_navn):
+        return (
+            kolonne_navn
+            .strip()
+            .lower()
+            .replace(' ', '_')
+            .replace('\xa0', '_')
+            .replace('\n', '')
+        )
+
+    def les_excel(self) -> pd.DataFrame:
+        df = pd.read_excel(io=self.filnavn, engine='openpyxl')
+        df.columns = [self.normaliser_kolonne(kol) for kol in df.columns]
+        self.normaliserte_kolonner = [self.normaliser_kolonne(kol) for kol in self.kolonner]
+
+        df_selected = df[self.normaliserte_kolonner]
+        df_selected.columns = self.kolonner
+        return df_selected
+    
+    def tell_antall(self, df: pd.DataFrame) -> Dict[str, List[int]]:
+        antall = {}
+        for kolonne in self.kolonner:
+            telling = (
+                df[kolonne]
+                .astype(str)
+                .value_counts()
+                .reindex(self.svar_alternativer, fill_value=0)
+            )
+            antall[kolonne] = telling.tolist()
+        return antall
+    
+    def langt_format(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.reset_index(names='idx')
+        df_lang = df.melt(id_vars=['idx'],
+                          value_vars=self.kolonner, 
+                          var_name='Spørsmål', 
+                          value_name='Svar')
+        return df_lang
+    
+
+class KolonneData(HighChartData):
+    
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel()
+        antall = self.tell_antall(df)
+        formatert_data = []
+        for kolonne, data in antall.items():
+            formatert_data.append({
+                'name': kolonne,
+                'type': 'column',
+                'data': data,
+                'colors': load_cmap("flattastic_flatui").colors,
+                'colorByPoint': True
+            })
+        return formatert_data
+    
+class StabletKolonneData(HighChartData):
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel()
+        antall = self.tell_antall(df)
+        formatert_data = []
+        for svar in self.svar_alternativer:
+            data = [antall[kolonne][self.svar_alternativer.index(svar)] for kolonne in self.kolonner]
+            formatert_data.append({
+                'name': svar,
+                'type': 'column',
+                'data': data,
+                'stack': 'Svar',
+                'color': load_cmap("flattastic_flatui").colors[self.svar_alternativer.index(svar) % len(load_cmap("flattastic_flatui").colors)]
+            })
+        return formatert_data
+    
+
+class ParallellData(HighChartData):
+    def __init__(self, filnavn: str, kolonner: List[str], svar_alternativer: Dict[str, List[str]] = None):
+        self.svar_alternativer = svar_alternativer or {}
+        super().__init__(filnavn, kolonner, self.svar_alternativer)
+
+    def langt_format(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.reset_index(names='idx')
+        df_lang = df.melt(id_vars=['idx'],
+                          value_vars=self.kolonner, 
+                          var_name='Spørsmål', 
+                          value_name='Svar')
+        return df_lang
+    
+    def map_responser_til_verdier(self, df_lang):
+        respons_mapping = {}
+        for kolonne in self.kolonner:
+            if kolonne in self.svar_alternativer:
+                categories = self.svar_alternativer[kolonne]
+            else:
+                categories = df_lang[df_lang['Spørsmål'] == kolonne]['Svar'].unique()
+            for idx, response in enumerate((categories)):
+                respons_mapping[response] = idx
+        
+        df_lang['MappedSvar'] = df_lang['Svar'].astype(str).map(respons_mapping)
+        self.respons_mapping = respons_mapping
+        return df_lang, respons_mapping
+    
+    def generer_parallell_koordinat_data(self, df_lang_med_mapping):
+        parallel_coordinates_data = []
+        for _, group in df_lang_med_mapping.groupby('idx'):
+            mapped_svar_list = group['MappedSvar'].tolist()
+            parallel_coordinates_data.append(mapped_svar_list)
+        
+        return parallel_coordinates_data
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel()
+        df = self.langt_format(df)
+        df, self.respons_mapping = self.map_responser_til_verdier(df)
+        data = self.generer_parallell_koordinat_data(df)
+        formatert_data = []
+        for i in range(len(data)):
+            formatert_data.append({
+                'name': 'Svar ' + str(i),
+                'data': data[i],
+            })
+        return formatert_data
+    
+
+class IndikatorData(HighChartData):
+    
+    def finn_gjennomsnitt(self, df: pd.Series) -> float:
+        return  df.mean().values[0]
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel()
+        gj_snitt = self.finn_gjennomsnitt(df)
+
+        formatert_data = [{
+                'data': gj_snitt,
+                'dataLabels': {
+                    'y': -30,
+                    'x': 0,
+                    'borderWidth': 0,
+                    'format':                 
+                    '<div style="display: flex; flex-direction: column; align-items: center; border: 1px solid red;">' +
+                    '<span style="font-size:70px; display: block; border: 1px solid blue;">{y:.1f}</span><br/>' +
+                    '</div>'
+                    },
+                'innerRadius': '90%',
+                'radius': '120%'
+            }]
+        return formatert_data
+    
+
+class PieData(HighChartData):
+
+    def finn_andel(self, df):
+        df = self.langt_format(df)
+
+        df['Svar'] = df['Svar'].apply(lambda x: x if x in self.svar_alternativer else 'Annet')
+
+        df_andel = df.groupby('Svar').agg(Antall=('Svar', 'count')).reset_index()
+
+        
+        df_andel['Prosent'] = (df_andel['Antall'] / df_andel['Antall'].sum()) * 100
+        df_andel['Prosent'] = df_andel['Prosent'].fillna(0)
+        df_andel['Svar'] = pd.Categorical(df_andel['Svar'], categories=self.svar_alternativer + ['Annet'], ordered=True)
+        df_andel = df_andel.sort_values('Svar')
+
+        data = df_andel[['Svar', 'Prosent']].rename(columns={'Svar': 'name', 'Prosent': 'y'}).to_dict(orient='records')
+        return data
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel()
+        data = self.finn_andel(df)
+        formatert_data = [{
+            'tooltip': {'value_suffix': '%', 'value_decimals': '1'},
+            'type': 'pie',
+            'name': 'Andel',
+            'colorByPoint': True,
+            'data': data,
+            'allowPointSelect': True,
+            'cursor': 'pointer',
+            'dataLabels': [{
+            'enabled': True,
+            'distance': 20
+            }, {
+            'enabled': True,
+            'distance': -40,
+            'format': '{point.percentage:.1f}%',
+            'style': {
+                'fontSize': '1.2em',
+                'textOutline': 'none',
+                'opacity': 0.7
+            },
+            'filter': {
+                'operator': '>',
+                'property': 'percentage',
+                'value': 10
+            }
+            }],
+            'showInLegend': False,
+        }]
+        return formatert_data
+    
+
+class BulletData(HighChartData):
+    def __init__(self, filnavn: str, kolonner: List[str], svar_alternativer: List[str], x_axis_categories: List[str] = None):
+        self.x_axis_categories = x_axis_categories or kolonner
+        super().__init__(filnavn, kolonner, svar_alternativer)
+
+    def gjennomsnitt_per_kolonne(self, df, spoersmaal):
+        df_spoersmaal = df[df['Spørsmål'] == spoersmaal]
+        return df_spoersmaal['Svar'].mean()
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel()
+        df_long = self.langt_format(df)
+        df_long['Svar'] = pd.to_numeric(df_long['Svar'], errors='coerce')
+
+        gj_snitt = [self.gjennomsnitt_per_kolonne(df_long, q) for q in self.kolonner]
+        farger = load_cmap("flattastic_flatui").colors
+
+        dataserier = [{'y': gj_snitt[i] if not pd.isna(gj_snitt[i]) else 0, 
+                        'target': 5, 'color': farger[5]} 
+                        for i in range(len(self.kolonner))]
+        return dataserier
+
+
+class JitterKommentarData(HighChartData):
+    def __init__(self, filnavn: str, 
+                 kolonner: Dict[str, str],
+                 svar_alternativer: List[str]):
+        self.svar_alternativer = svar_alternativer
+        self.kolonner = kolonner
+        self.score = self.kolonner.get('score', 'score')
+        self.label = self.kolonner.get('label', 'label')
+        self.kommentar = self.kolonner.get('kommentar', 'kommentar')
+        self.df = self.df_from_pickle(filnavn)
+        self.dataserier = self.lag_dataserier()
+    
+    def df_from_pickle(self, filnavn):
+        with open(filnavn, 'rb') as f:
+            df = pickle.load(f)
+        return df
+
+
+    def lag_jitter_data(self, df, x_value):
+        jitter_data = []
+        for _, row in df.iterrows():
+            jitter_x = x_value + (0.7 * (0.5 - np.random.rand()))
+            jitter_y = row[self.score]
+            jitter_data.append({
+                'x': jitter_x,
+                'y': jitter_y,
+                'custom': {
+                    'kommentar': row[self.kommentar][:30] + '...',
+                }
+            })
+        return jitter_data
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.df
+        colors = load_cmap("flattastic_flatui").colors
+        colors = [colors[3], colors[2], colors[1], colors[6]]
+        dataserie = []
+        for i, label in enumerate(self.svar_alternativer):
+            df_filtered = df[df[self.label] == label]
+            jitter_data = self.lag_jitter_data(df_filtered, i + 1)
+            dataserie.append({
+                'name': f'{label}',
+                'type': 'scatter',
+                'data': jitter_data,
+                'marker': {
+                    'radius': 5,
+                    'symbol': 'circle'
+                },
+                'color': colors[i],
+                'tooltip': {
+                    'pointFormat': 'Sikkerhet: {point.y:.2f} <br>{point.custom.kommentar}'
+                },
+            })
+        return dataserie
+    
+
+class KommentarData(HighChartData):
+    def __init__(self, filnavn: str, 
+                kolonner: Dict[str, str],
+                svar_alternativer: List[str]):
+        self.filnavn = filnavn
+        self.svar_alternativer = svar_alternativer
+        self.kolonner = kolonner
+        self.dataserier = self.lag_dataserier()
+    
+    def formater_kommentar_linjeskift(self, comment, max_length=50):
+        words = comment.split()  # Split into words
+        formatted_comment = ''
+        current_line_length = 0
+        for word in words:
+            if current_line_length + len(word) + 1 > max_length:
+                formatted_comment += '<br>' 
+                current_line_length = 0  
+            formatted_comment += word + ' '  
+            current_line_length += len(word) + 1  
+        return formatted_comment.strip()  
+
+    def lag_dataserier(self) -> List[Dict]:
+        df = self.les_excel().dropna().reset_index()
+        df = df.sample(frac=1).reset_index(drop=True)
+        colors = load_cmap("flattastic_flatui").colors
+        colors = [colors[6]]
+        
+        # Iterate over each column in kolonner and apply the formatting function
+        for col in self.kolonner:
+            df[col] = df[col].apply(lambda x: self.formater_kommentar_linjeskift(x))
+
+        # Prepare data for Highcharts
+        jitter_data = []
+        for index, row in df.iterrows():
+            for col in self.kolonner:
+                jitter_data.append({
+                    'svar_nr': int(index) + 1,
+                    'x': random.random(),  
+                    'y': random.random(),  
+                    'kommentar': row[col]
+                })
+
+        # Create data series
+        dataserie = []
+        dataserie.append({
+            'type': 'scatter',
+            'data': jitter_data,
+            'marker': {
+                'radius': 5,
+                'symbol': 'circle'
+            },
+            'color': colors[0],
+        })
+
+        return dataserie
--- src/ung_plotteverktoey/plots.py
+++ src/ung_plotteverktoey/plots.py
@@ -1,16 +1,281 @@
-import matplotlib.pyplot as plt
+import uuid
+from highcharts_core.chart import Chart
+from highcharts_core.options import HighchartsOptions
+from highcharts_core.utility_classes.javascript_functions import CallbackFunction
+from pypalettes import load_cmap
+import json
 
-# Sample data
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 5, 7, 11]
 
-# Create a simple plot
-plt.plot(x, y, marker='o')
+class HighChartBase:
+    def __init__(self, 
+                 data, 
+                 tittel: str = " ", 
+                 undertittel: str = " ", 
+                 y_akse_tekst: str = " "
+                 ):
+        self.data = data
+        self.tittel = tittel
+        self.undertittel = undertittel
+        self.y_akse_tekst = y_akse_tekst
+        self.diagram_id = str(uuid.uuid4())
+        self.innstillinger = self.definer_innstillinger()
 
-# Add title and labels
-plt.title('Simple Plot')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+    def lag_diagram(self):
+        diagram = Chart(options=self.innstillinger)
+        diagram.display()
 
-# Show the plot
-plt.show()
+
+class KolonneDiagram(HighChartBase): 
+    def definer_innstillinger(self):
+        innstillinger = HighchartsOptions(
+            chart={'renderTo': self.diagram_id, 'type': 'column'},
+            title={'text': self.tittel},
+            subtitle={'text': self.undertittel},
+            x_axis={'categories': self.data.svar_alternativer},
+            y_axis={'title': {'text': self.y_akse_tekst}},
+            series=self.data.dataserier,
+            credits={'enabled': False}
+            )
+        return innstillinger
+
+
+class StabletKolonneDiagram(HighChartBase):
+    def definer_innstillinger(self):
+        innstillinger = HighchartsOptions(
+            chart={'renderTo': self.diagram_id, 'type': 'column'},
+            title={'text': self.tittel},
+            subtitle={'text': self.undertittel},
+            x_axis={'categories': self.data.kolonner},
+            y_axis={'title': {'text': self.y_akse_tekst}},
+            series=self.data.dataserier,
+            credits={'enabled': False},
+            plot_options={'column': {'stacking': 'percent', 'dataLabels': {'enabled': True, 'format': '{point.percentage:.0f}%'}}},
+            tooltip={'pointFormat': '<span style="color:{point.color}">{series.name}</span>: <b>{point.percentage:.1f}%</b> <br>Antall: {point.y:.0f}'}
+        )
+        return innstillinger
+    
+
+class ParallellDiagram(HighChartBase):
+    def definer_innstillinger(self):
+        y_axis = []
+        for kolonne in self.data.kolonner:
+            if kolonne in self.data.svar_alternativer:
+                kategorier = self.data.svar_alternativer[kolonne]
+                y_axis.append({'categories': kategorier,
+                'tickPositions': [x for x, _ in enumerate(kategorier)],
+                    } )
+            else:
+                kategorier = list(self.data.respons_mapping.keys())
+                y_axis.append({'categories': [str(cat) for cat in kategorier],
+                    } )
+        
+
+
+        innstillinger = HighchartsOptions(
+            chart={
+                'renderTo': self.diagram_id,
+                'type': 'spline',
+                'parallelCoordinates': True,
+                'parallelAxes': {
+                    'lineWidth': 2,
+                },
+                },
+            title={'text': self.tittel},
+            plot_options={
+                'series': {
+                    'allowPointSelect': False,
+                    'lineWidth': 2,
+                    'states': {
+                        'inactive': {
+                            'opacity': 0.1,
+                        },
+                    },
+                    'point': {
+                        'events': {
+                            'mouseOver': '''function() {
+                                var series = this.series.chart.series,
+                                    x = this.x,
+                                    y = this.y;
+
+                                Highcharts.each(series, function(s) {
+                                    if (s.data[x].y === y) {
+                                        s.setState('hover');
+                                    } else {
+                                        s.setState('inactive');
+                                    }
+                                });
+                            }''',
+                            'mouseOut': '''function() {
+                                var series = this.series.chart.series;
+                                Highcharts.each(series, function(s) {
+                                    s.setState('');
+                                });
+                            }''',
+                        }
+                    }
+                }
+            },
+            tooltip={'pointFormat': """<span style="color:{point.color}; font-size: 10">\\u25CF</span><b>{point.formattedValue}</b>"""},
+            x_axis={'categories': self.data.kolonner, 'offset': 10},
+            y_axis=y_axis,
+            colors=['rgba(11, 200, 200, 0.06)'],
+            credits={'enabled': False},
+            series=self.data.dataserier,
+        )
+        return innstillinger
+    
+
+class IndikatorDiagram(HighChartBase):
+    def definer_innstillinger(self):
+        farger = load_cmap("flattastic_flatui").colors
+        innstillinger = HighchartsOptions(
+            chart={'renderTo': self.diagram_id, 'type': 'solidgauge'},
+            title={'text': self.tittel},
+            subtitle={'text': self.undertittel, 'y': 90},
+            pane={'background': 
+                  {'backgroundColor': '#fff', 'innerRadius': '90%', 'outerRadius': '120%', 'shape': 'arc'}, 
+                  'center': ['50%', '70%'], 'endAngle': 90, 'startAngle': -90},
+            series=self.data.dataserier,
+            tooltip={'enabled': False},
+            y_axis={
+                'labels': {'distance': 40}, 'max': 10, 'min': 0, 'stops': [
+                    [0.0, farger[0]],  
+                    [0.4, farger[2]], 
+                    [0.7, farger[3]]], 
+                'minorTickInterval': 20,
+                'tickAmount': 6,
+                'tickLength': '-40',
+                'tickWidth': 0,
+                'visible': True,
+                'title': {'y': -70},
+                'lineColor': 'white'
+            },
+            credits={'enabled': False}
+        )
+        return innstillinger
+    
+
+class PieDiagram(HighChartBase):
+
+    def definer_innstillinger(self):
+        innstillinger = HighchartsOptions(
+            chart={'renderTo': self.diagram_id, 'type': 'pie'},
+            title={'text': self.tittel},
+            subtitle={'text': self.undertittel},
+            series=self.data.dataserier,
+            credits={'enabled': False},
+            plot_options={
+        'tooltip': {'value_suffix': '%', 'value_decimals': '1'},
+        'type': 'pie',
+        'name': 'Andel',
+        'colorByPoint': True,
+        'allowPointSelect': True,
+        'cursor': 'pointer',
+        'dataLabels': [{
+            'enabled': True,
+            'distance': 20
+        }, {
+            'enabled': True,
+            'distance': -40,
+            'format': '{point.percentage:.1f}%',
+            'style': {
+                'fontSize': '1.2em',
+                'textOutline': 'none',
+                'opacity': 0.7
+            },
+            'filter': {
+                'operator': '>',
+                'property': 'percentage',
+                'value': 10
+            }
+        }],
+        'showInLegend': True,
+    },
+            tooltip= {'value_suffix': '%', 'value_decimals': '1'},
+        )
+        return innstillinger
+    
+
+class BulletDiagram(HighChartBase):
+    def definer_innstillinger(self):
+        options = HighchartsOptions(
+            chart={'renderTo': self.diagram_id, 
+                   'inverted': True, 
+                   'margin_left': 135, 
+                   'margin_right': 100, 
+                   'type': 'bullet'},
+            title={'text': self.tittel},
+            legend={'enabled': False},
+            y_axis={'grid_line_width': 0.5, 
+                    'min': 0, 
+                    'max': 5, 
+                    'tick_interval': 1, 
+                    'title': {'text': ''}, 
+                    'labels': {'formatter': "function() { return '⭐'.repeat(Math.floor(this.value)); }"}},
+            credits={'enabled': False},
+            exporting={'enabled': False},
+            x_axis={'categories': self.data.x_axis_categories},
+            series=[{'data': self.data.dataserier}],
+            tooltip={'point_format': '<b>{point.y:.2f}</b> av {point.target}'}
+        )
+        return options
+
+
+class JitterKommentarDiagram(HighChartBase):
+    def definer_innstillinger(self):
+        options = HighchartsOptions(
+            chart={'renderTo': self.diagram_id, 'type': 'scatter'},
+            title={'text': self.tittel},
+            legend={'enabled': True},
+            x_axis={
+                'title': {'text': 'Polaritet'},
+                'categories': [0, 'Positive', 'Mixed', 'Negative', 'Neutral']
+            },
+            y_axis={
+                'title': {'text': 'Sikkerhet'},
+            },
+            series=self.data.dataserier
+        )
+        return options
+
+
+
+
+class KommentarDiagram(HighChartBase):
+
+
+    def tooltip_formatter(self):
+        chart_data_json = json.dumps(self.data.dataserier[0]['data'])  # No need to manually escape quotes
+        # Define the JavaScript function for the tooltip formatter
+        formatter_js = f"""
+        function myTooltipFormatter() {{
+            var chartData = JSON.parse('{chart_data_json.replace("'", "\\'").replace('"', '\\"')}');
+            var pointData = chartData.find(row => row.x === this.point.x);
+            return '<b>Svar nr:</b> ' + pointData.svar_nr + '<br>' +
+                '<b>Kommentar:</b> ' + '<br>' + pointData.kommentar;
+        }}
+        """
+        tooltip_formatter = CallbackFunction.from_js_literal(formatter_js)
+        return tooltip_formatter
+
+
+    def definer_innstillinger(self):
+        innstillinger = HighchartsOptions(
+            chart={
+                'type': 'scatter'
+            },
+            title={'text': self.tittel},
+            subtitle={'text':self.undertittel},
+            tooltip={
+                'formatter': self.tooltip_formatter()
+            },
+            series=[{
+                'name': 'Kommentar',
+                'data': [[row['x'], row['y']] for row in self.data.dataserier[0]['data']] 
+            }],
+            x_axis={'min': -0.3, 'max':1.3, 'visible': False},
+            y_axis={'title': {'text': ' '}, 'visible': False},
+            credits={'enabled': False},
+
+        )
+        return innstillinger
